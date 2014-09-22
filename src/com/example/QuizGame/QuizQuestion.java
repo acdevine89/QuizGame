8283f4e240c1e88b80f83ce2f4139bab499c48ac
acdevine89 <annie.devine@detroitlabs.com>
2014-09-22 16:10:05 -0400

    Adding constructors, getters and setters to QuizQuestion class

--- src/com/example/QuizGame/QuizQuestion.java
+++ src/com/example/QuizGame/QuizQuestion.java
@@ -1,7 +1,74 @@
 package com.example.QuizGame;
 
-/**
- * Created by anniedevine on 9/22/14.
- */
 public class QuizQuestion {
+
+    private String question;
+    private String wordAnswer;
+    private double numericAnswer;
+    private boolean trueOrFalseAnswer;
+    private char multipleChoiceAnswer;
+
+    public QuizQuestion(String question, String wordAnswer)
+    {
+        this.question = question;
+        this.wordAnswer = wordAnswer;
+    }
+
+    public QuizQuestion(String question, double numericAnswer)
+    {
+        this.question = question;
+        this.numericAnswer = numericAnswer;
+    }
+
+    public QuizQuestion(String question, boolean trueOrFalseAnswer)
+    {
+        this.question = question;
+        this.trueOrFalseAnswer = trueOrFalseAnswer;
+    }
+
+    public QuizQuestion(String question, char multipleChoiceAnswer)
+    {
+        this.question = question;
+        this.multipleChoiceAnswer = multipleChoiceAnswer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getWordAnswer() {
+        return wordAnswer;
+    }
+
+    public void setWordAnswer(String wordAnswer) {
+        this.wordAnswer = wordAnswer;
+    }
+
+    public double getNumericAnswer() {
+        return numericAnswer;
+    }
+
+    public void setNumericAnswer(double numericAnswer) {
+        this.numericAnswer = numericAnswer;
+    }
+
+    public boolean isTrueOrFalseAnswer() {
+        return trueOrFalseAnswer;
+    }
+
+    public void setTrueOrFalseAnswer(boolean trueOrFalseAnswer) {
+        this.trueOrFalseAnswer = trueOrFalseAnswer;
+    }
+
+    public char getMultipleChoiceAnswer() {
+        return multipleChoiceAnswer;
+    }
+
+    public void setMultipleChoiceAnswer(char multipleChoiceAnswer) {
+        this.multipleChoiceAnswer = multipleChoiceAnswer;
+    }
 }
